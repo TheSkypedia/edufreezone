38a41fa62e664eb44d44737a2cfcae010233517d
Saipreetham <39019648+Saipreetham0@users.noreply.github.com>
2023-09-17 00:36:51 +0530

    update

--- app/components/Card/index.js
+++ app/components/Card/index.js
@@ -3,57 +3,61 @@
 import React from "react";
 import Image from "next/image";
 
-const Card = ({ title, content, buttonText, url, category, badge }) => {
+const Card = ({ title, content, image, buttonText, url, category, badge }) => {
   return (
-    <div  className="max-w-sm  bg-white border border-gray-200 hover:border-blue-400 hover:dark:border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm  bg-white border border-gray-200 hover:border-blue-400 hover:dark:border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       {/* <a href={url}>You can add an image here if needed</a> */}
-      <div className="p-5">
-        <div className="p-2 mb-2 flex justify-between">
-          <Image src="/favicon.ico" alt="" width={80} height={50} />
+      <div className="p-5 ">
+        <div className=" ">
+          <div className="p-2 mb-2 flex justify-between">
+            <Image src={image} alt="" width={80} height={50} />
 
-          <div className="p-5">
-            <span className="inline-flex items-center rounded-md bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800">
-              {badge}
-            </span>
+            <div className="p-5">
+              <span className=" inline-flex items-center rounded-md bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800">
+                {badge}
+              </span>
+            </div>
           </div>
+
+          <a href="#">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {title}
+            </h5>
+          </a>
+
+          <p className="mb-3    font-normal text-gray-700 dark:text-gray-400">
+            {content}
+          </p>
         </div>
 
-        <a href="#">
-          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {title}
-          </h5>
-        </a>
-
-        <p className="mb-3    font-normal text-gray-700 dark:text-gray-400">
-          {content}
-        </p>
-        {/* <div className="flex  items-center mb-5 text-gray-500">
-          <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        <div>
+          <div className="flex  items-center mb-5 text-gray-500">
+            {/* <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
             {category}
-          </span>
-        </div> */}
-        <a
-          href={url}
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          {/* {buttonText} */}
-          Read more
-          <svg
-            className="w-3.5 h-3.5 ml-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
+          </span> */}
+          </div>
+          <a
+            href={url}
+            className="mt-6 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
+            Read more
+            <svg
+              className="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -6,29 +6,152 @@ import Hero from "./components/Hero";
 
 const cardsData = [
   {
-    title: "Noteworthy technology acquisitions 2021",
+    title: "Amazon Prime Student",
     content:
-      " Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      "Amazon is a global online marketplace that offers a wide range of products and services. Students can enjoy a free trial of Amazon Prime Student for six months, gaining access to fast and free shipping, streaming services, and exclusive discounts. Available worldwide.",
     buttonText: "Button 1 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/amazon-logo.webp",
     category: "Category A",
-    url: "http://sai",
+    url: "https://www.amazon.in/gp/prime/pipeline/youngadult/ref=s9_acss_bw_cg_PYAPC_md1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-1&pf_rd_r=QMQEEFMA01QBWSBQCHWC&pf_rd_t=101&pf_rd_p=d7529bad-a3ed-4b5e-98bc-bfb39aad8121&pf_rd_i=15307611031&tag=saipreetham07-21&sid=KfK3KK",
     badge: "Free",
   },
   {
-    title: "Card 2 Title",
-    content: "Content for Card 2",
+    title: "Github Student Developer Pack",
+    content:
+      "GitHub: GitHub is a web-based platform for version control and collaboration that allows developers to work together on projects. GitHub’s Student Developer Pack is a collection of tools and services available to current students.",
     buttonText: "Button 2 Text",
     category: "Category B",
-    url: "http://sai",
+    url: "https://education.github.com/pack",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/github-logo.webp",
+    badge: "Free",
+  },
+  {
+    title: "JetBrains",
+    content:
+      "JetBrains provides professional developer software and integrated development environments. Students can access these tools for free for 12 months, enhancing their productivity and code quality. Available worldwide.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/jetbrains-logo.webp",
+    category: "Category B",
+    url: "https://www.jetbrains.com/community/education/#students",
+  },
+  {
+    title: "HP",
+    content:
+      "HP offers student store discounts, enabling current students to access special deals on technology products. Available worldwide.",
+    buttonText: "Button 2 Text",
+    image: "https://theskypedia.com/wp-content/uploads/2023/06/hp-logo.webp",
+    category: "Category B",
+    url: "https://www.hp.com/in-en/shop/students/",
+  },
+  {
+    title: "Autodesk",
+    content:
+      "Autodesk offers free design and modeling software for 36 months, empowering students worldwide to bring their creative ideas to life.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/autodesk-logo.webp",
+    category: "Category B",
+    url: "https://www.autodesk.com/education/free-software/",
+  },
+  {
+    title: "Amazon Web Services",
+    content:
+      "AWS Educate Program provides free access to Amazon Web Services for 12 months, allowing students worldwide to gain hands-on experience with cloud computing technologies. (NEW DEAL!)",
+    buttonText: "Button 2 Text",
+    image: "https://theskypedia.com/wp-content/uploads/2023/06/aws-logo.webp",
+    category: "Category B",
+    url: "https://aws.amazon.com/education/awseducate/",
+  },
+  {
+    title: "Razer",
+    content:
+      "Razer’s education program offers a 15% discount on their products for current students in the EU and USA, providing high-quality gaming hardware and accessories. Free for current students.",
+    buttonText: "Button 2 Text",
+    image: "https://theskypedia.com/wp-content/uploads/2023/06/razer-logo.webp",
+    category: "Category B",
+    url: "https://www.razer.com/education",
+  },
+  {
+    title: "Microsoft Office",
+    content:
+      "Microsoft Office 365 Software is available for free to current students worldwide, providing essential productivity tools for academic and personal use.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/office-logo.webp",
+    category: "Category B",
+    url: "https://www.microsoft.com/en-gb/education/products/office",
+  },
+  {
+    title: "Basecamp",
+    content:
+      "Basecamp offers free project management software to current students worldwide, helping them collaborate and organize their projects effectively.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/basecamp-logo.webp",
+    category: "Category B",
+    url: "https://basecamp.com/discounts",
+  },
+  {
+    title: "Lucidchart",
+    content:
+      "Lucidchart provides free flowchart software to current students worldwide, aiding them in visualizing and communicating ideas and processes.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/lucidchart-logo.webp",
+    category: "Category B",
+    url: "https://www.lucidchart.com/pages/usecase/education",
+  },
 
-    badge: "Free",
+  {
+    title: "Microsoft Azure",
+    content:
+      "Microsoft Azure offers free cloud computing credits for current students worldwide, allowing them to explore and develop cloud-based applications. (NEW DEAL!)",
+    buttonText: "Button 2 Text",
+    image: "https://theskypedia.com/wp-content/uploads/2023/06/azure-logo.webp",
+    category: "Category B",
+    url: "https://azure.microsoft.com/free/students",
   },
   {
-    title: "Card 3 Title",
-    content: "Content for Card 2",
+    title: "Apple",
+    content:
+      "Apple’s Education Pricing Program provides special discounts on Apple products for current students worldwide, making technology more accessible for educational purposes.    ",
     buttonText: "Button 2 Text",
+    image: "https://theskypedia.com/wp-content/uploads/2023/06/apple-logo.webp",
     category: "Category B",
-    url: "http://sai",
+    url: "https://www.apple.com/in-edu/store",
+  },
+  {
+    title: "Lucidchart",
+    content:
+      "Lucidchart provides free flowchart software to current students worldwide, aiding them in visualizing and communicating ideas and processes.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/lucidchart-logo.webp",
+    category: "Category B",
+    url: "https://www.lucidchart.com/pages/usecase/education",
+  },
+  {
+    title: "Lucidchart",
+    content:
+      "Lucidchart provides free flowchart software to current students worldwide, aiding them in visualizing and communicating ideas and processes.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/lucidchart-logo.webp",
+    category: "Category B",
+    url: "https://www.lucidchart.com/pages/usecase/education",
+  },
+  {
+    title: "Lucidchart",
+    content:
+      "Lucidchart provides free flowchart software to current students worldwide, aiding them in visualizing and communicating ideas and processes.",
+    buttonText: "Button 2 Text",
+    image:
+      "https://theskypedia.com/wp-content/uploads/2023/06/lucidchart-logo.webp",
+    category: "Category B",
+    url: "https://www.lucidchart.com/pages/usecase/education",
   },
 ];
 
